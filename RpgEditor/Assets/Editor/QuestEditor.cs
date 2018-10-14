16dff8c8eabf1745c17cc15f479118323e8e6ad5
TheFachoATR <44141137+TheFachoATR@users.noreply.github.com>
2018-10-14 14:06:45 -0300

    asd

---
+++ RpgEditor/Assets/Editor/QuestEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(Quest))]
+public class QuestEditor : Editor {
+
+    private Quest _quest;
+
+    private void OnEnable()
+    {
+        _quest = (Quest)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("UN LABEL");
+        _quest.questDescription = EditorGUILayout.TextField(_quest.questDescription);
+    }
+}
---
+++ RpgEditor/Assets/Quests/Scripts/Quest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    public string questTitle;
+    public string questDescription;
+    public int experienceGained;
+    public int creditsGained;
+	
+
+
+
+
+}
