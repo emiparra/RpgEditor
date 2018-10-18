d90f746c4e70d6397b808cecee9a48789be71480
emiparra <44100896+emiparra@users.noreply.github.com>
2018-10-18 16:38:31 -0300

    limpio sin

--- RpgEditor/Assets/Editor/QuestNodeWindow.cs
+++ RpgEditor/Assets/Editor/QuestNodeWindow.cs
@@ -13,13 +13,7 @@ public class QuestNodeWindow : EditorWindow
     private Node _SelectedNode;
     string Nname;
 
-    private bool _panningScreen;
-    private Vector2 graphpan;
-    private Vector2 _originalMousePosition;
-    private Vector2 prevPan;
-    private Rect graphRect;
-    
-    public GUIStyle Wrapstyle;
+
 	[MenuItem("RPG/Quest Nodes")]
 	
     public static void OpenWindow()
@@ -31,11 +25,7 @@ public class QuestNodeWindow : EditorWindow
         Nodewindow.style.alignment = TextAnchor.MiddleLeft;
         Nodewindow.style.fontStyle = FontStyle.Normal;
 
-        Nodewindow.graphpan = new Vector2(0, Nodewindow.toolbarHeight);
-        Nodewindow.graphRect = new Rect(0, Nodewindow.toolbarHeight, 10000, 10000);
-
-        Nodewindow.Wrapstyle = new GUIStyle(EditorStyles.textField);
-        Nodewindow.Wrapstyle.wordWrap = true;
+     
     }
 
     private void OnGUI()
@@ -44,7 +34,6 @@ public class QuestNodeWindow : EditorWindow
         EditorGUILayout.LabelField("Node Name");
         Nname= GUILayout.TextField(Nname);
         
-        CheckMouse(Event.current);
         if (GUILayout.Button("Create Node", GUILayout.Width(toolbarHeight), GUILayout.Height(30)))
         {
             if(Nname == "")
@@ -60,67 +49,19 @@ public class QuestNodeWindow : EditorWindow
         
         EditorGUILayout.EndHorizontal();
         BeginWindows();
-        var oricol = GUI.backgroundColor;
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-            foreach (var c in allNodes[i].Connected)
-            {
-                Handles.DrawLine(new Vector2(allNodes[i].rect.position.x + allNodes[i].rect.width / 2f, allNodes[i].rect.position.y + allNodes[i].rect.height / 2f), new Vector2(c.rect.width / 2f, c.rect.position.y + c.rect.height / 2f));
-            }
-        }
+     
+     
         for (int i = 0; i < allNodes.Count; i++)
         {
             if (allNodes[i] == _SelectedNode)
                 GUI.backgroundColor = Color.black;
             allNodes[i].rect = GUI.Window(i, allNodes[i].rect, Drawnode, allNodes[i].NodeName);
-            GUI.backgroundColor = oricol;
+         
         }
         EndWindows();
         
     }
-    private void CheckMouse(Event E)
-    {
-        if (!graphRect.Contains(E.mousePosition) || !(focusedWindow == this) || mouseOverWindow == this)
-            return;
-        if (E.button == 2 && E.type == EventType.MouseDown)
-        {
-            _panningScreen = true;
-            prevPan = new Vector2(graphpan.x, graphpan.y);
-            _originalMousePosition = E.mousePosition;
-        }
-        else if (E.button == 2 && E.type == EventType.MouseUp)
-            _panningScreen = false;
-
-        if(_panningScreen)
-        {
-            var newX = prevPan.x + E.mousePosition.x - _originalMousePosition.x;
-            graphpan.x = newX > 0 ? 0 : newX;
-
-            var newY = prevPan.y + E.mousePosition.y - _originalMousePosition.y;
-            graphpan.y = newY > toolbarHeight ? toolbarHeight : newY;
-            Repaint();
-        }
-        Node overNode = null;
-
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-            allNodes[i].CheckMouse(Event.current, graphpan);
-            if (allNodes[i].OverNode)
-                overNode = allNodes[i];
-        }
-        var prevSel = _SelectedNode;
-        if(E.button == 0 && E.type == EventType.MouseDown)
-        {
-            if (overNode != null)
-                _SelectedNode = overNode;
-            else
-                _SelectedNode = null;
-            if (prevSel != _SelectedNode)
-                Repaint();
-
-        }
-        
-    }
+  
     private void AddNode()
     {
         CurrentName = Nname;
@@ -130,7 +71,7 @@ public class QuestNodeWindow : EditorWindow
     }
     private void Drawnode(int id)
     {
-        //  EditorGUILayout.ObjectField("OB",Node.Quest);
+       
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("Quest:", GUILayout.Width(50));
         Node.Quest = (GameObject)EditorGUILayout.ObjectField(Node.Quest,typeof(GameObject),false);
@@ -139,18 +80,13 @@ public class QuestNodeWindow : EditorWindow
         {
             
             allNodes.RemoveAt(id);
-            allNodes = allNodes.GetRange(0, allNodes.Count - 1);
+           
 
         }
 
-            if (!_panningScreen)
-        {
+           
             GUI.DragWindow();
 
-            if (allNodes[id].rect.x < 0)
-                allNodes[id].rect.x = 0;
-            if (allNodes[id].rect.y < toolbarHeight - graphpan.y)
-                allNodes[id].rect.y = toolbarHeight - graphpan.y;
-        }
+      
     }
 }
