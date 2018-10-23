dc12dc28ffbfa4ea9557e41fede2c8f6f1dfd1f1
emiparra <44100896+emiparra@users.noreply.github.com>
2018-10-22 21:06:44 -0300

    apa
    asd

--- RpgEditor/Assets/Editor/QuestNodeWindow.cs
+++ RpgEditor/Assets/Editor/QuestNodeWindow.cs
@@ -52,15 +52,23 @@ public class QuestNodeWindow : EditorWindow
         BeginWindows();
 
         var col = GUI.backgroundColor;
-      
+        for (int i = 0; i < allNodes.Count; i++)
+        {
+            foreach (var Q in allNodes[i].Connected)
+            {
+
+            }
+        }
+
         for (int i = 0; i < allNodes.Count; i++)
         {
             if (allNodes[i] == _SelectedNode)
                 GUI.backgroundColor = Color.red;
+
             allNodes[i].rect = GUI.Window(i, allNodes[i].rect, Drawnode, allNodes[i].NodeName);
             GUI.backgroundColor = col;
         }
-        
+
         EndWindows();
         GUI.EndGroup();
         
@@ -70,12 +78,53 @@ public class QuestNodeWindow : EditorWindow
 
     private void CheckMouse(Event currentE)
     {
-     
+        if (!graphrect.Contains(currentE.mousePosition) || !(focusedWindow == this || mouseOverWindow == this))
+            return;
+
+        if (currentE.button == 2 && currentE.type == EventType.MouseDown)
+        {
+            panninscreen = true;
+            prevPan = new Vector2(Graphpan.x, Graphpan.y);
+            OriginalMousePosition = currentE.mousePosition;
+
+        }
+        else if (currentE.button == 2 && currentE.type == EventType.MouseUp)
+            panninscreen = false;
+        if(panninscreen)
+        {
+            var newX = prevPan.x + currentE.mousePosition.x - OriginalMousePosition.x;
+            Graphpan.x = newX > 0 ? 0 : newX;
+
+            var newY = prevPan.y + currentE.mousePosition.y - OriginalMousePosition.y;
+            Graphpan.y = newY > toolbarHeight ? toolbarHeight : newY;
+
+            Repaint();
+
+        }
+
         if (currentE.button == 1 && currentE.type == EventType.MouseDown && graphrect.Contains(currentE.mousePosition))
             ContextMenu();
 
-        
-        
+        Node overnode = null;
+        for (int i = 0; i < allNodes.Count; i++)
+        {
+            allNodes[i].CheckMouse(Event.current, Graphpan);
+            if (allNodes[i].OverNode)
+                overnode = allNodes[i];
+
+        }
+        var prev = _SelectedNode;
+        if (currentE.button == 0 && currentE.type == EventType.MouseDown)
+        {
+            if (overnode != null)
+                _SelectedNode = overnode;
+            else
+                _SelectedNode = null;
+            if (prev != _SelectedNode)
+                Repaint();
+                    
+                    
+                    }
 
         
        
