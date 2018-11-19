6a0c9e6b5a0198daf12305c463d3bf702cf8712b
Axel <44141137+TheFachoATR@users.noreply.github.com>
2018-11-19 19:56:39 -0300

    new nodes

--- RpgEditor/Assets/Nodes/Node.cs
+++ RpgEditor/Assets/Nodes/Node.cs
@@ -1,73 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System;
-public class Node : ScriptableObject
- {
+
+public class Node:EditorWindow
+{ 
     public Rect rect;
+    public string title;
+    public bool isDragged;
+    public bool isSelected;
     public string NodeName;
-    public  QuestData Quest;
-    public ParamsData Param;
-    private bool _overnode;
-    public bool complete;
-    public List<Node> Connected;
-    public bool StartNode;
-    public bool FinishNode;
-    public bool ConditionNode;
-    public string Knows;
-    public bool selected;
-    public Node(float x, float y, float width, float height, string name)
-    {
-        rect = new Rect(x, y, width, height);
-        Connected = new List<Node>();
-       
-        NodeName = name;
-     
-    }
+    public ConnectionPoint inPoint;
+    public ConnectionPoint outPoint;
+
+    public GUIStyle style;
+    public GUIStyle defaultNodeStyle;
+    public GUIStyle selectedNodeStyle;
+
+    public Action<Node> OnRemoveNode;
+    public UnityEngine.Object nn;
    
-    public void CheckMouse(Event cE, Vector2 pan)
-    {
-        if (rect.Contains(cE.mousePosition - pan))
-            _overnode = true;
 
-        else
-            _overnode = false;
-    }
-    public bool OverNode
-    {
-        get { return _overnode; }
-    }
-
-
-
-
-
-
-
-
-    public void checkQuest()
+    public Node(Vector2 position, float width, float height, GUIStyle nodeStyle, GUIStyle selectedStyle, GUIStyle inPointStyle, GUIStyle outPointStyle, Action<ConnectionPoint> OnClickInPoint, Action<ConnectionPoint> OnClickOutPoint, Action<Node> OnClickRemoveNode)
     {
        
+        rect = new Rect(position.x+10, position.y+10, width+100, height+100);
+        style = nodeStyle;
+        inPoint = new ConnectionPoint(this, ConnectionPointType.In, inPointStyle, OnClickInPoint);
+        outPoint = new ConnectionPoint(this, ConnectionPointType.Out, outPointStyle, OnClickOutPoint);
+        defaultNodeStyle = nodeStyle;
+        selectedNodeStyle = selectedStyle;
+        OnRemoveNode = OnClickRemoveNode;
+    }
 
-        if ( complete == true)
+    public void Drag(Vector2 delta)
+    {
+        rect.position += delta;
+    }
+
+    public void Draw()
+    {
+
+       
+        inPoint.Draw();
+        outPoint.Draw();
+        GUI.Box(rect, title, style);
+       // EditorGUI.DrawRect(rect, Color.black);
+        GUI.BeginGroup(rect);
+        BeginWindows();
+        
+        nn = (UnityEngine.Object)EditorGUILayout.ObjectField(nn, typeof(UnityEngine.Object), false);
+        
+        EndWindows();
+        GUI.EndGroup();
+    }
+
+    public bool ProcessEvents(Event e)
+    {
+        switch (e.type)
         {
-            Connected.Add(this);
+            case EventType.MouseDown:
+                if (e.button == 0)
+                {
+                    if (rect.Contains(e.mousePosition))
+                    {
+                        isDragged = true;
+                        GUI.changed = true;
+                        isSelected = true;
+                        style = selectedNodeStyle;
+                    }
+                    else
+                    {
+                        GUI.changed = true;
+                        isSelected = false;
+                        style = defaultNodeStyle;
+                    }
+                }
+                if (e.button == 1 && isSelected && rect.Contains(e.mousePosition))
+                {
+                    ProcessContextMenu();
+                    e.Use();
+                }
+                break;
+
+            case EventType.MouseUp:
+                isDragged = false;
+                break;
+
+            case EventType.MouseDrag:
+                if (e.button == 0 && isDragged)
+                {
+                    Drag(e.delta);
+                    e.Use();
+                    return true;
+                }
+                break;
         }
-        else if (complete == false)
+        return false;
+    }
+    private void ProcessContextMenu()
+    {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Remove node"), false, OnClickRemoveNode);
+        genericMenu.ShowAsContext();
+    }
+
+    private void OnClickRemoveNode()
+    {
+        if (OnRemoveNode != null)
         {
-            if(Connected.Contains(this))
-            {
-
-                Connected.Remove(this);
-            }
+            OnRemoveNode(this);
         }
-
-     
-
-
     }
 
 
-    
-   
+
+
 }
